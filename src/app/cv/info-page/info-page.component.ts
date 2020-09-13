7d39a755bedfbba6ddb1ee145b9e5be8d1863bbc
Luizun <luisacosta360@gmail.com>
2020-09-13 02:46:22 -0500

    Se diseña componente caracteristicas

--- src/app/cv/info-page/info-page.component.ts
+++ src/app/cv/info-page/info-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { faAngular, faNodeJs } from '@fortawesome/free-brands-svg-icons';
-import { faTerminal } from '@fortawesome/free-solid-svg-icons';
+import { faAngular, faNodeJs, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faTerminal, faServer } from '@fortawesome/free-solid-svg-icons';
 import { CvService } from 'src/app/services/cv.service';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-info-page',
@@ -10,10 +11,14 @@ import { CvService } from 'src/app/services/cv.service';
 })
 export class InfoPageComponent implements OnInit {
 
+  url: string = `${environment.urlApi}/api/info/exp`;
+
   // icons
   iconAngular = faAngular;
   iconNode = faNodeJs;
   iconTerminal = faTerminal;
+  iconServer = faServer;
+  iconGithub = faGithub;
 
   constructor(
     public cvSrv: CvService
