1f350eabc510fbd144a3f602845053759f571c66
Luis Acosta <luis.acosta@gnp.com.mx>
2021-03-23 09:45:01 -0600

    Se agrega base de datos en uso

--- src/app/cv/info-page/info-page.component.ts
+++ src/app/cv/info-page/info-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faAngular, faNodeJs, faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faTerminal, faServer } from '@fortawesome/free-solid-svg-icons';
+import { faTerminal, faServer, faDatabase } from '@fortawesome/free-solid-svg-icons';
 import { CvService } from 'src/app/services/cv.service';
 import { environment } from '../../../environments/environment';
 
@@ -19,6 +19,7 @@ export class InfoPageComponent implements OnInit {
   iconTerminal = faTerminal;
   iconServer = faServer;
   iconGithub = faGithub;
+  iconDataBase = faDatabase;
 
   constructor(
     public cvSrv: CvService
